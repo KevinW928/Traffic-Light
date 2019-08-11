170816888da018a6ce2a4e6e6d8e72f32af2c760
KevinW928 <52473604+KevinW928@users.noreply.github.com>
2019-08-11 16:18:12 -0400

    Update traffic.js

--- traffic.js
+++ traffic.js
@@ -7,27 +7,32 @@ var n = 0;  // state inital variable in colour sequence //
 // var Red = document.getElementById("red"); // 
 // var Yellow = document.getElementById("yellow"); //
 //  Green = document.getElementById("green"); // 
+						
+					        // PROBLEM - SEE BELOW // 
+
+	      // Starting at red, want to increment by 1 to get to green. Once it hits green, decrement back down to red //  
+
+			               // Have no idea what I'm doing below :-(  //
 
 
-														// Starting at red, want to increment by 1 to get to green. Once it hits 			green, decrement back down to red //  
-
-																// Have no idea what I'm doing below :-(  //
 
 function colour_change() {
  
  // when n goes to the last colour, will decrease count back down to red //
 	
  	if ( n == 3) {
-						green.style.backgroundColor = colours[n];
-						yellow.style.backgroundColor = colours[n - 1];
-						Red.style.backgroundColor = colours[n - 2];
-	 } else {
-						red.style.backgroundColor = colours[n];
-						yellow.style.backgroundColor = colours[n + 1];
-						green.style.backgroundColor = colours[n + 2];
+		green.style.backgroundColor = colours[n];
+		yellow.style.backgroundColor = colours[n - 1];
+		Red.style.backgroundColor = colours[n - 2];
+	} 
+	
+	
+	else {
+		red.style.backgroundColor = colours[n];
+		yellow.style.backgroundColor = colours[n + 1];
+		green.style.backgroundColor = colours[n + 2];
 	}
 		
-	
 }
 
 colour_change() // call function //
